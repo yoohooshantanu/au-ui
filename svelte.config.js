be30f25f749f704cb33575b24bc213ce25e323fc
yoohooshantanu <santanujuvekar@gmail.com>
2026-06-18 13:08:16 +0530

    Add base path /dashboard to SvelteKit config

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,10 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			out: 'build'
-		})
+		}),
+		paths: {
+			base: '/dashboard'
+		}
 	}
 };
 
